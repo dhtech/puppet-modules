de4f01a826475646462f075389c4f288a1da2cc9
Sebastian Svensson <ss@tinbox.nu>
2018-10-25 16:35:15 +0200

    Lint docker module

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -27,8 +27,8 @@ class docker {
     path    => '/etc/apt/sources.list.d/docker.list',
     content => 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable',
     notify  => Exec['docker-source-key'],
-  }->
-  exec { 'docker-source-key':
+  }
+  -> exec { 'docker-source-key':
     command     => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | /usr/bin/apt-key add -',
     logoutput   => 'on_failure',
     try_sleep   => 1,
@@ -51,8 +51,8 @@ class docker {
   file { '/etc/sysctl.d/dh-docker.conf':
     ensure  => 'file',
     content => 'net.bridge.bridge-nf-call-iptables=1',
-  }~>
-  exec { 'refresh-sysctl':
+  }
+  ~> exec { 'refresh-sysctl':
     command     => '/sbin/sysctl --system',
     refreshonly => true,
   }
@@ -62,9 +62,9 @@ class docker {
   }
 
   file { '/etc/docker/daemon.json':
-     ensure  => file,
-     content => template('docker/daemon.json.erb'),
-     notify  => Package['docker-ce'],
+      ensure  => file,
+      content => template('docker/daemon.json.erb'),
+      notify  => Package['docker-ce'],
   }
 
 }
