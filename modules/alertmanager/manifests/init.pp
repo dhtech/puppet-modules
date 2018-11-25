546f9da10c455dcd929b1fe5609f39e3aca8b1c7
Andreas Holmsten <andreas.holmsten@gmail.com>
2018-11-25 18:11:27 +0100

    Begin breakout of dhmon services

---
+++ modules/alertmanager/manifests/init.pp
@@ -0,0 +1,14 @@
+# Copyright 2018 dhtech
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file
+#
+# == Class: alertmanager
+#
+# Alert manager for prometheus to handle sending alerts
+#
+# === Parameters
+#
+
+class alertmanager {
+}
