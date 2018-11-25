3c3f70df90ad2ce8e4e5386be8720ae8b4356469
Rasmus Jönsson <rasmus@rasmusj.se>
2018-11-25 18:18:27 +0100

    etcd initial commit

---
+++ modules/etcd/manifests/init.pp
@@ -0,0 +1,38 @@
+# Copyright 2018 dhtech
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file
+#
+# == Class: etcd::init
+#
+# Used for initializing etcd cluster.
+#
+# === Parameters
+#
+
+class etcd::init($variant = "default", $nodes = []) {
+  # TODO (rctl): vault["etcd_${variant}:ca", {}] -> /etc/etcd/ca.crt
+  #              vault["etcd_${variant}:client", {}] -> /etc/etcd/client.pem
+  #              vault["etcd_${variant}:key", {}] -> /etc/etcd/key.pem
+  #              vault["etcd_${variant}:peercert", {}] -> /etc/etcd/peer.pem
+  #              vault["etcd_${variant}:peerkey", {}] -> /etc/etcd/key.pem
+  #              vault["etcd_${variant}:token", {}] -> $token
+  
+  file { 'etcd-unit':
+    path    => '/etc/systemd/system/etcd.service',
+    ensure  => file,
+    content => template('etcd/etcd.service.erb'),
+    notify  => Exec['daemon-reload'],
+  }
+
+  exec { 'daemon-reload':
+    command     => '/bin/systemctl daemon-reload':
+    refreshonly => true,
+    notify     => Exec['daemon-restart'],
+  }
+
+  exec { 'daemon-restart':
+    command     => '/bin/systemctl restart etcd':
+    refreshonly => true,
+  }
+}
---
+++ modules/etcd/manifests/install.pp
@@ -0,0 +1,54 @@
+# Copyright 2018 dhtech
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file
+#
+# == Class: etcd::install
+#
+# Used for installing/setting up all dependencies for etcd.
+#
+# === Parameters
+#
+
+class etcd::install {
+  # 
+  # Install basic packages
+  ensure_packages([
+      'curl',
+  ])
+
+  file { '/etc/etcd':
+    ensure => directory,
+  }
+  
+  exec { 'etcd-download':
+    command     => '/usr/bin/curl https://github.com/etcd-io/etcd/releases/download/v3.3.10/etcd-v3.3.10-linux-amd64.tar.gz',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    notify      => Exec['etcd-unpack'],
+  }
+
+  exec { 'etcd-unpack':
+    command     => '/usr/bin/tar -xvf etcd-v3.3.10-linux-amd64.tar.gz',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    notify      => Exec['etcd-install'],
+  }
+
+  exec { 'etcd-install':
+    command     => 'cp ./etcd-v3.3.10-linux-amd64/etcd /usr/bin/etcd',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    notify      => Exec['etcd-clean'],
+  }
+
+  exec { 'etcd-clean':
+    command     => '/usr/bin/rm -rf ./etcd-v3.3.10-linux-amd64',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+  }
+}
