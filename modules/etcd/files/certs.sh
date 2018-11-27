ea8efeac1fe4a14b3db70df6f0f7d8aa38e24ff5
Rasmus Jönsson <rasmus@rtek.se>
2018-11-27 20:31:08 +0100

    Etcd for dhw18 (#84)
    * etcd initial commit
    
    * Remove token when using peercert
    
    * Fix missing variable variant
    
    * Fix bad pp syntax
    
    * Change copy file method
    
    * Fix bad dependency
    
    * Use absolute paths
    
    * Fix node listing from ipplan
    
    * Fix node listing
    
    * Fix erb tmpl for etcd
    
    * Include self in etcd hosts
    
    * Add missing linebreak
    
    * Curl follow redirect
    
    * Fix paths
    
    * Fix file permissions
    
    * Filemode as string
    
    * Fix etcd flags
    
    * Use https for etcd
    
    * Only install etcd when it is missing
    
    * Change to service block for etcd
    
    * Generate cert for etcd
    
    * Generate peering certs
    
    * Require cert for unit
    
    * Fix bad format
    
    * Fix circular dep
    
    * Fix requirement
    
    * Add req
    
    * Fix dep
    
    * Ensure present
    
    * Do not clear src
    
    * Use install dir
    
    * Wrong param
    
    * Install
    
    * Fix param
    
    * Fix cert path
    
    * Set peering as client cert
    
    * Use fqdn for node name
    
    * Fetch kube api cert
    
    * Do not append host to nodelist
    
    * Remove whitespace
    
    * Properly indent
    
    * Puppet format
    
    * Lint

---
+++ modules/etcd/files/certs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+VAULT_ADDR="https://vault.tech.dreamhack.se:1443/"
+
+# Login with our machine account
+RESPONSE=$(curl -ks \
+  -H "Content-Type: application/json" \
+  --data @<(echo '{"name":"puppet-etcd"}') \
+  --cacert /etc/ssl/certs/ca-certificates.crt \
+  --cert /var/lib/puppet/ssl/certs/$(hostname -f).pem \
+  --key /var/lib/puppet/ssl/private_keys/$(hostname -f).pem \
+  ${VAULT_ADDR}v1/auth/cert/login -XPOST)
+
+# Extract token
+echo "Logging in to vault server $VAULT_ADDR"
+TOKEN=$(echo $RESPONSE | python -c "
+import sys
+import json
+
+print json.loads(sys.stdin.read())['auth']['client_token']")
+
+CA='/etc/ssl/etcd-ca.crt'
+CERTFILE="/etc/ssl/etcd-$(hostname).crt"
+KEYFILE="/etc/ssl/private/etcd-$(hostname).key"
+
+# Install certificates
+if [ "$(hostname -d)" == "tech.dreamhack.se" ]; then
+  TTL="42720h" # 5y
+else
+  TTL="720h" # 30d
+fi
+
+echo "Using TTL $TTL"
+
+if [ ! -f ${CERTFILE} ]; then
+  echo "Installing missing certificates"
+  echo "-H \"X-Vault-Token: $TOKEN\"" |
+    curl --data @<(echo "{\"ttl\": \"${TTL}\", \"common_name\": \"$(hostname -f)\"}") \
+      --cacert /etc/ssl/certs/ca-certificates.crt \
+      -s -X POST -K - ${VAULT_ADDR}v1/etcd-pki/issue/peering | python -c "
+import sys
+import json
+
+response = sys.stdin.read()
+print response
+result = json.loads(response)['data']
+open('${CERTFILE}', 'w').write(result['certificate'])
+open('${KEYFILE}', 'w').write(result['private_key'])
+open('${CA}', 'w').write(result['issuing_ca'])
+"
+fi
+
---
+++ modules/etcd/manifests/init.pp
@@ -0,0 +1,45 @@
+# Copyright 2018 dhtech
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file
+#
+# == Class: etcd::init
+#
+# Used for initializing etcd cluster.
+#
+# === Parameters
+#
+
+class etcd::init($variant = 'default', $nodes = []) {
+
+  $trustedclient =  vault("kube-${variant}:apicert")
+
+  file { 'dh-etcd-peering':
+    ensure => file,
+    path   => '/usr/bin/dh-etcd-peering',
+    mode   => '0755',
+    source => 'puppet:///modules/etcd/certs.sh',
+    notify => Exec['etcd-peering-cert'],
+  }
+
+  exec { 'etcd-peering-cert':
+    command     => '/usr/bin/dh-etcd-peering',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    notify      => File['etcd-unit'],
+  }
+
+  file { 'etcd-unit':
+    ensure  => file,
+    path    => '/etc/systemd/system/etcd.service',
+    content => template('etcd/etcd.service.erb'),
+    notify  => Service['etcd-server'],
+    require => Exec['etcd-peering-cert'],
+  }
+
+  service { 'etcd-server':
+    ensure => 'running',
+    enable => true,
+  }
+}
