fc5a4fda09816d93b38ad1f2c5b500ab3369dc86
Rasmus Jönsson <rasmus@rtek.se>
2018-11-29 12:10:09 +0100

    Fix init exec (#127)
    * Fix init exec
    
    * Format

--- modules/kubernetes/manifests/worker.pp
+++ modules/kubernetes/manifests/worker.pp
@@ -16,13 +16,13 @@ class kubernetes::worker($variant, $apiserver, $cert_hash, $token) {
     ensure  => 'file',
     content => template('kubernetes/join.yaml.erb'),
     notify  => Exec['join-cluster'],
+    require => [Package['kubeadm'], Exec['k8s-disable-swap']],
   }
 
   exec { 'join-cluster':
     command     => '/usr/bin/kubeadm join --config /etc/kubernetes/kubeadm-config.yaml',
     creates     => '/etc/kubernetes/kubelet.conf',
     refreshonly => true,
-    require     => [Package['kubeadm'], Exec['k8s-disable-swap']],
   }
 
 }
