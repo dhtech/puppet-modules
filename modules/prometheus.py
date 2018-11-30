137e35d0f9ef408efab95432af91dc5f9cace227
Sebastian Svensson <ss@tinbox.nu>
2018-11-30 12:54:20 +0100

    Adding vCenter scrapes to prometheus (#154)
    * Adding vcenter scrapes to prometheus

--- modules/prometheus.py
+++ modules/prometheus.py
@@ -131,6 +131,15 @@ def generate_backend(host, local_services):
     }
     scrape_configs.append(external)
 
+    vcenter = {
+      'job_name': 'vmware_vcenter',
+      'metrics_path': '/metrics',
+      'static_configs': [{
+          'targets': ['http://provision.event.dreamhack.se:9272'],
+      }],
+    }
+    scrape_configs.append(vcenter)
+
     return {'scrape_configs': scrape_configs}
 
 
