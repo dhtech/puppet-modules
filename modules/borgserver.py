551930ad74eba7053b40f165b11ac9b42d76e19f
Christian Svensson <blue@cmd.nu>
2019-01-26 15:59:15 +0100

    Initial Borg server

---
+++ modules/borgserver.py
@@ -0,0 +1,16 @@
+# Copyright 2019 dhtech
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file
+import pypuppetdb
+
+def generate(host, *args):
+    db = pypuppetdb.connect()
+    ssh = db.facts('ssh')
+    servers = [{
+        'fqdn': x.node,
+        'ssh-key': '%s %s' % (x.value['ecdsa']['type'], x.value['ecdsa']['key'])
+        } for x in ssh]
+    return {'borgserver': {'servers': servers}}
+
+# vim: ts=4: sts=4: sw=4: expandtab
