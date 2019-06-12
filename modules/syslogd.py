3f0274d9892bf185c72d1c2af25d8b1576686c75
Viktor Ekmark <viktor@ekmark.se>
2019-06-12 15:50:34 +0200

    fixing some more on syslogd (#212)

--- modules/syslogd.py
+++ modules/syslogd.py
@@ -2,12 +2,19 @@
 #
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file
-
+import lib
 
 def generate(host, *args):
+
+    ircbots = sorted(lib.get_nodes_with_package('ircbot', 'event').keys())
+
     info = {}
     if 'rsyslog' in args:
-        info['rsyslog'] = {}
+        if ircbots:
+            info['rsyslog'] = {'ircbot': ircbots[0]}
+        else
+            info['rsyslog'] = {}
+
     return info
 
 # vim: ts=4: sts=4: sw=4: expandtab
