2feb97d43570b07e69a90269329d238552bde43b
Christian Svensson <blue@cmd.nu>
2019-11-24 22:28:23 +0100

    Y

--- modules/ldaplogin/manifests/init.pp
+++ modules/ldaplogin/manifests/init.pp
@@ -51,8 +51,8 @@ class ldaplogin ($ca, $logon, $sudo, $ldap, $ssh_ports, $panic_users,
   }
 
   service { ['sssd-nss.socket', 'sssd-autofs.socket', 'sssd-pac.socket',
-             'sssd-pam-priv.socket', 'sssd-ssh.socket', 'sssd-sudo.socket']:
-    ensure => 'disable',
+    'sssd-pam-priv.socket', 'sssd-ssh.socket', 'sssd-sudo.socket']:
+    ensure   => 'disable',
     provider => 'systemd',
  }
 
