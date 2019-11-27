ca7591adcc2cfff4933afc3a2ed320575460868d
Alexander Kratzsch <klump@devrandom.se>
2019-11-27 20:29:35 +0100

    Fix error in enc script

--- modules/observer.py
+++ modules/observer.py
@@ -8,6 +8,7 @@ import lib
 def generate(host):
     info = {}
 
+    resolvers = lib.get_servers_for_node('eventdns', host)
     ips = lib.resolve_nodes_to_ip(resolvers)
     info['nameservers'] = [ips[x][0] for x in resolvers]
 
