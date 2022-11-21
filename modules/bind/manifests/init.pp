13a2e833e3995f0347ee7a4d68c0843884c19ebb
Markus Häll <soundgoof@gmail.com>
2022-11-21 20:36:40 +0100

    Update init.pp
    Deploy bind exporter without scripts

--- modules/bind/manifests/init.pp
+++ modules/bind/manifests/init.pp
@@ -142,6 +142,14 @@ class bind($role='resolver', $networks = [], $zones = [], $private_zones = [],
     require => Package[$package_name],
   }
 
+  file { 'bind_exporter_binary':
+    ensure => file,
+    path   => '/usr/sbin/bind_exporter',
+    source => puppet:///data/bind_exporter,
+    links  => follow,
+    notify => [ Service['bind_exporter'] ],
+  }
+
   file { 'bind_exporter.service':
     ensure  => file,
     path    => '/etc/systemd/system/bind_exporter.service',
@@ -149,11 +157,13 @@ class bind($role='resolver', $networks = [], $zones = [], $private_zones = [],
   }
   ~> exec { '/bin/systemctl daemon-reload':
     refreshonly => true,
+    notify      => [ Service['bind_exporter'] ],
   }
-  ~> exec { '/bin/systemctl restart bind_exporter':
-    logoutput   => 'on_failure',
-    try_sleep   => 1,
-    refreshonly => true,
+  
+  service { 'bind_exporter':
+    ensure  => 'running',
+    enable  => true,
+    require => File['bind_exporter_binary'],
   }
 
   file { 'named.conf.slave':
