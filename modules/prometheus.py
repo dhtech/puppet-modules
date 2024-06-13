42c9b12b99ce9e620ac58016797d0b14f9419205
Alexander Malmström <jag@alexandermalmstrom.se>
2024-06-13 17:05:04 +0200

    Missed one bool, and some tabs on row 158

--- modules/prometheus.py
+++ modules/prometheus.py
@@ -145,7 +145,7 @@ def generate_backend(host, local_services):
         }
         scrape_configs.append(puppet)
 
-        if lib.check_node_active("provision.event.dreamhack.se") == "true":
+        if lib.check_node_active("provision.event.dreamhack.se"):
             vcenter = {
                 'job_name': 'vmware_vcenter',
                 'metrics_path': '/metrics',
@@ -155,7 +155,7 @@ def generate_backend(host, local_services):
                     'targets': ['provision.event.dreamhack.se:9272'],
                 }],
             }
-    scrape_configs.append(vcenter)
+            scrape_configs.append(vcenter)
 
     # Make sure that all metrics have a host label.
     # This rule uses the existing host label if there is one,
