fa969bcedb651c992b095f47051d86119cbba4d0
furest <furest@furest.be>
2024-06-22 11:08:27 +0200

    Major improvements and fixes

--- modules/akvorado.py
+++ modules/akvorado.py
@@ -10,6 +10,68 @@ import yaml
 
 DB_FILE = '/etc/ipplan.db'
 
+def get_sflow_clients():
+    if os.path.isfile(DB_FILE):
+        try:
+            conn = sqlite3.connect(DB_FILE)
+            db = conn.cursor()
+        except sqlite3.Error as e:
+            print("An error occurred: {}".format(e.args[0]))
+            exit(2)
+    else:
+        print("No database file found: {}".format(DB_FILE))
+        exit(3)
+    db.execute(
+            "SELECT h.name AS hostname, h.ipv4_addr_txt AS ipv4_addr ,h.ipv6_addr_txt AS ipv6_addr, o2.value AS layer "
+            "FROM host h "
+            "INNER JOIN option o1 ON h.node_id = o1.node_id "
+            "INNER JOIN option o2 ON h.node_id = o2.node_id "
+            "WHERE o1.name='pkg' AND o1.value='sflowclient' "
+            "AND o2.name='layer'"
+            )
+    res = db.fetchall()
+    if not res:
+        return None
+
+    column_names = [description[0] for description in db.description]
+    conn.close()
+    rows_dict = [dict(zip(column_names, row)) for row in res]
+
+    return rows_dict
+
+def get_snmpv2_providers():
+    providers = []
+    clients = get_sflow_clients()
+    current_event = lib.get_current_event()
+    for client in clients:
+        key = current_event+'-mgmt/snmp:'+client['layer']
+        secrets = lib.read_secret(key)
+        if "community" in secrets:
+            provider = {
+                    "ipv4": client["ipv4_addr"],
+                    "community": secrets["community"],
+                    }
+            providers.append(provider)
+    return providers
+
+def get_snmpv3_providers():
+    providers = []
+    clients = get_sflow_clients()
+    current_event = lib.get_current_event()
+    for client in clients:
+        key = current_event+'-mgmt/snmp:'+client['layer']
+        secrets = lib.read_secret(key)
+        if "user" in secrets:
+            provider = {
+                    "ipv4": client["ipv4_addr"],
+                    "authentication-passphrase": secrets["auth"],
+                    "authentication-protocol": secrets["authtype"].replace(" ","").upper(),
+                    "privacy-passphrase": secrets["priv"],
+                    "privacy-protocol": secrets["privtype"].replace(" ","").replace("128","").upper(),
+                    "user": secrets["user"],
+                    }
+            providers.append(provider)
+    return providers
 
 def get_prefixes(ipversion):
     if os.path.isfile(DB_FILE):
@@ -60,6 +122,8 @@ def requires(host, *args):
 def generate(host, *args):
 
     info = {}
+    info['snmpv3_providers'] = get_snmpv3_providers()
+    info['snmpv2_providers'] = get_snmpv2_providers()
     info['current_event'] = lib.get_current_event()
     info['ipv6_prefixes'] = get_prefixes('6')
     info['ipv4_prefixes'] = get_prefixes('4')
--- modules/akvorado/manifests/init.pp
+++ modules/akvorado/manifests/init.pp
@@ -10,15 +10,155 @@
 # === Parameters
 #
 
-class akvorado ($current_event, $ipv4_prefixes, $ipv6_prefixes) {
+class akvorado ($current_event, $ipv4_prefixes, $ipv6_prefixes, $snmpv3_providers, $snmpv2_providers) {
+
+
+  ##Kafka installation  
+  ensure_packages([
+    'openjdk-17-jre',
+  ])
+  group { 'kafka':
+    ensure => 'present',
+  }
+  -> user { 'kafka':
+    ensure     => 'present',
+    system     => true,
+    home       => '/var/lib/kafka',
+    managehome => true,
+  }
+  -> file { '/var/lib/kafka/kafka.tgz':
+    ensure  => file,
+    links   => follow,
+    source  => 'puppet:///data/kafka-latest.tgz',
+    notify  => Exec['untar-kafka']
+  }
+  -> file { '/var/log/kafka':
+    ensure => 'directory',
+    owner  => 'kafka',
+    group  => 'kafka',
+    mode   => '0700',
+  }
+  -> file { '/var/lib/zookeeper-data':
+    ensure => 'directory',
+    owner  => 'kafka',
+    group  => 'kafka',
+    mode   => '0700',
+  }
+  exec { 'untar-kafka':
+    command     => '/bin/tar -xvf /var/lib/kafka/kafka.tgz -C /var/lib/kafka --strip 1',
+    refreshonly => 'true',
+    user        => 'kafka',
+  }
+  file { '/etc/systemd/system/kafka.service':
+    ensure => present,
+    source => 'puppet:///modules/akvorado/kafka.service',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    notify => [ Exec['systemctl-daemon-reload'], Service['kafka'] ],
+  }
+  -> file { '/etc/systemd/system/zookeeper.service':
+    ensure => present,
+    source => 'puppet:///modules/akvorado/zookeeper.service',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    notify => [ Exec['systemctl-daemon-reload'], Service['zookeeper'] ],
+  }
+  -> file_line { 'kafka-enabledeletetopics':
+    ensure  => 'present',
+    path    => '/var/lib/kafka/config/server.properties',
+    line    => 'delete.topic.enable = true',
+    notify  => Service['kafka'],
+  }
+  -> file_line { 'kafka-listenlocalhost':
+    ensure  => 'present',
+    path    => '/var/lib/kafka/config/server.properties',
+    line    => 'listeners=PLAINTEXT://localhost:9092',
+    match   => '#listeners=PLAINTEXT',
+    notify  => Service['kafka'],
+  }
+  -> file_line { 'kafka-logdir':
+    ensure  => 'present',
+    path    => '/var/lib/kafka/config/server.properties',
+    line    => 'log.dirs=/var/log/kafka',
+    match   => 'log.dirs=/tmp/kafka-logs',
+    notify  => Service['kafka'],
+  }
+  -> file_line { 'zookeeper-datadir':
+    ensure  => 'present',
+    path    => '/var/lib/kafka/config/zookeeper.properties',
+    line    => 'dataDir=/var/lib/zookeeper-data',
+    match   => 'dataDir=/tmp/zookeeper',
+    notify  => Service['zookeeper'],
+  }
+  -> file_line { 'zookeeper-listen':
+    ensure  => 'present',
+    path    => '/var/lib/kafka/config/zookeeper.properties',
+    line    => 'clientPortAddress=127.0.0.1',
+    notify  => Service['zookeeper'],
+  }
+  service { 'kafka':
+    ensure  => running,
+    enable  => true,
+  }
+  service { 'zookeeper':
+    ensure  => running,
+    enable  => true,
+  }
+
+  ##Clickhouse installation
+  ensure_packages([
+    'apt-transport-https',
+    'ca-certificates',
+    'curl',
+    'gnupg',
+  ])
+  file { 'clickhouse-source-add':
+    ensure  => file,
+    path    => '/etc/apt/sources.list.d/clickhouse.list',
+    content => 'deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main',
+    notify  => Exec['clickhouse-source-key'],
+  }
+  exec { 'clickhouse-source-key':
+    command     => '/usr/bin/curl -fsSL https://packages.clickhouse.com/rpm/lts/repodata/repomd.xml.key | gpg --dearmor > /usr/share/keyrings/clickhouse-keyring.gpg',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    notify      => Exec['apt-update'],
+  }
+  exec { 'apt-update':
+    command     => '/usr/bin/apt-get update',
+    logoutput   => 'on_failure',
+    try_sleep   => 1,
+    refreshonly => true,
+    require     => Package['apt-transport-https'],
+  }
+
+  package { 'clickhouse-server':
+    ensure  => installed,
+    require => [File['clickhouse-source-add'], Exec['clickhouse-source-key'], Exec['apt-update']],
+  }
+  -> package { 'clickhouse-client':
+    ensure  => installed,
+  }
+  -> service { 'clickhouse-server':
+    ensure  => running,
+    enable  => true,
+  }
 
   #Create user/group for Akvorodo
+  ensure_packages([
+    'redis',
+  ])
   group { 'akvorado':
     ensure => 'present',
       }
   -> user { 'akvorado':
     ensure => 'present',
     system => true,
+    home       => '/var/lib/akvorado',
+    managehome => true,
   }
   #Create directories for akvorado
   -> file { '/etc/akvorado':
@@ -35,8 +175,8 @@ class akvorado ($current_event, $ipv4_prefixes, $ipv6_prefixes) {
     mode   => '0550',
     links  => follow,
     source => 'puppet:///data/akvorado-latest',
+    notify => [Service['akvorado-orch'],Exec['protobuf-schema']]
   }
-
   file { '/etc/akvorado/akvorado.yaml':
     ensure  => file,
     content => template('akvorado/akvorado.yaml.erb'),
@@ -67,143 +207,53 @@ class akvorado ($current_event, $ipv4_prefixes, $ipv6_prefixes) {
     group  => 'root',
     notify => [Exec['systemctl-daemon-reload'],Service['akvorado-console']],
   }
-
-  -> apache::proxy { 'akvorado':
+  file { '/usr/share/GeoIP':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file { '/usr/share/GeoIP/asn.mmdb':
+    ensure => present,
+    source => 'puppet:///data/asn.mmdb',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+  file { '/usr/share/GeoIP/country.mmdb':
+    ensure => present,
+    source => 'puppet:///data/country.mmdb',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+  apache::proxy { 'akvorado':
     url     => '/',
     backend => 'http://localhost:8082/',
   }
   -> service { 'akvorado-orch':
     ensure  => running,
+    enable  => true,
   }
   -> service { 'akvorado-inlet':
     ensure  => running,
+    enable  => true,
   }
   -> service { 'akvorado-console':
     ensure  => running,
+    enable  => true,
   }
-
+  -> service { 'redis':
+    ensure  => running,
+    enable  => true,
+  }
   exec { 'systemctl-daemon-reload':
     command     => '/bin/systemctl daemon-reload',
     refreshonly => true,
   }
-
-  ##Kafka installation  
-  group { 'kafka':
-    ensure => 'present',
-  }
-  -> user { 'kafka':
-    ensure     => 'present',
-    system     => true,
-    home       => '/var/lib/kafka',
-    managehome => true,
-  }
-  -> file { '/tmp/kafka.tgz':
-    ensure => file,
-    links  => follow,
-    source => 'puppet:///data/kafka-latest.tgz',
-  }
-  -> file { '/var/log/kafka':
-    ensure => 'directory',
-    owner  => 'kafka',
-    group  => 'kafka',
-    mode   => '0700',
-  }
-  -> file { '/etc/systemd/system/kafka.service':
-    ensure => present,
-    source => 'puppet:///modules/akvorado/kafka.service',
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    notify => [ Exec['systemctl-daemon-reload'], Service['kafka'] ],
-  }
-  -> file { '/etc/systemd/system/zookeeper.service':
-    ensure => present,
-    source => 'puppet:///modules/akvorado/zookeeper.service',
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    notify => [ Exec['systemctl-daemon-reload'], Service['zookeeper'] ],
-  }
-  -> file_line { 'kafka-enabledeletetopics':
-    ensure  => 'present',
-    path    => '/var/lib/kafka/config/server.properties',
-    line    => 'delete.topic.enable = true',
-    match   => 'delete.topic.enable',
-    notify  => Service['kafka'],
-    require => Exec['untar-kafka'],
-  }
-  -> file_line { 'kafka-listenlocalhost':
-    ensure  => 'present',
-    path    => '/var/lib/kafka/config/server.properties',
-    line    => 'listeners=PLAINTEXT://localhost:9092',
-    match   => '#listeners=PLAINTEXT',
-    notify  => Service['kafka'],
-    require => Exec['untar-kafka'],
-  }
-  -> file_line { 'kafka-logdir':
-    ensure  => 'present',
-    path    => '/var/lib/kafka/config/server.properties',
-    line    => 'log.dirs=/var/log/kafka',
-    match   => 'log.dirs=',
-    notify  => Service['kafka'],
-    require => Exec['untar-kafka'],
-  }
-  -> file_line { 'zookeeper-listen':
-    ensure  => 'present',
-    path    => '/var/lib/kafka/config/zookeeper.properties',
-    line    => 'clientPortAddress=127.0.0.1',
-    match   => 'clientPortAddress=',
-    notify  => Service['zookeeper'],
-    require => Exec['untar-kafka'],
-  }
-  exec { 'untar-kafka':
-    command     => '/bin/tar -xvf /tmp/kafka.tgz -C /var/lib/kafka --strip 1',
+  exec { 'protobuf-schema':
+    command     => '/usr/bin/curl http://127.0.0.1:8080/api/v0/orchestrator/clickhouse/init.sh | sh',
     refreshonly => true,
-    user        => 'kafka',
-  }
-  -> service { 'kafka':
-    ensure  => running,
-  }
-  -> service { 'zookeeper':
-    ensure  => running,
-  }
-
-  ##Clickhouse installation
-  ensure_packages([
-    'apt-transport-https',
-    'ca-certificates',
-    'curl',
-    'gnupg',
-  ])
-  file { 'clickhouse-source-add':
-    ensure  => file,
-    path    => '/etc/apt/sources.list.d/clickhouse.list',
-    content => 'deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main',
-    notify  => Exec['clickhouse-source-key'],
-  }
-  exec { 'clickhouse-source-key':
-    command     => '/usr/bin/wget -fsSL https://packages.clickhouse.com/rpm/lts/repodata/repomd.xml.key -O /usr/share/keyrings/clickhouse-keyring.gpg',
-    logoutput   => 'on_failure',
-    try_sleep   => 1,
-    refreshonly => true,
-    notify      => Exec['apt-update'],
-  }
-  exec { 'apt-update':
-    command     => '/usr/bin/apt-get update',
-    logoutput   => 'on_failure',
-    try_sleep   => 1,
-    refreshonly => true,
-    require     => Package['apt-transport-https'],
-  }
-
-  package { 'clickhouse-server':
-    ensure  => installed,
-    require => [File['clickhouse-source-add'], Exec['clickhouse-source-key'], Exec['apt-update']],
-  }
-  -> package { 'clickhouse-client':
-    ensure  => installed,
-  }
-  -> service { 'clickhouse-server':
-    ensure  => running,
+    require     => Service['akvorado-orch']
   }
 }
