633c0cc068b18601ac10c516ad3d9dd571b8c9c2
Robin Johansson <reth@reth.se>
2024-07-02 20:03:50 +0200

    fix: remove static hostnames from paths and use puppet ssl certs

--- modules/request/manifests/init.pp
+++ modules/request/manifests/init.pp
@@ -17,9 +17,10 @@
 #
 
 class request {
-  $dhid   = vault('app:request.tech.dreamhack.se', {})
-  $dbname = 'request.tech.dreamhack.se'
-  $db     = vault("postgresql:${dbname}", {})
+  $dhid    = vault("app:${::fqdn}", {})
+  $dbname  = $::fqdn
+  $db      = vault("postgresql:${dbname}", {})
+  $webroot = "/var/www/${::fqdn}"
 
   ensure_packages([
     'apache2', 'php', 'php-cli', 'libapache2-mod-php', 'php-gd', 'php-xml',
@@ -51,24 +52,35 @@ class request {
     require => File['install_composer.sh'],
   }
 
-  file { '/etc/apache2/sites-available/request.tech.dreamhack.se.conf':
+  file { "/etc/apache2/sites-available/${::fqdn}.conf":
     notify  => Exec['a2ensite'],
     mode    => '0644',
     owner   => 'www-data',
     group   => 'www-data',
     content => template('request/request.tech.dreamhack.se.conf.erb'),
+    require => [
+      File['/etc/ssl/certs/server-fullchain.crt'],
+      File['/etc/ssl/private/server.key']
+    ],
+  }
+
+  exec { 'a2enmod':
+    path    => [ '/bin', '/usr/bin', '/usr/sbin' ],
+    command => 'a2enmod ssl',
+    creates => '/etc/apache2/mods-enabled/ssl.conf',
+    notify  => Service['apache2'],
+    require => Package['apache2'],
   }
 
   exec { 'a2ensite':
-    refreshonly => true,
-    path        => [ '/bin', '/usr/bin', '/usr/sbin' ],
-    command     => 'a2ensite request.tech.dreamhack.se.conf',
-    creates     => '/etc/apache2/sites-enabled/request.tech.dreamhack.se.conf',
-    notify      => Service['apache2'],
-    require     => File['/var/www/request.tech.dreamhack.se'],
+    path    => [ '/bin', '/usr/bin', '/usr/sbin' ],
+    command => "a2ensite ${::fqdn}.conf",
+    creates => "/etc/apache2/sites-enabled/${::fqdn}.conf",
+    notify  => Service['apache2'],
+    require => File[$webroot],
   }
 
-  file { '/var/www/request.tech.dreamhack.se':
+  file { $webroot:
     ensure  => directory,
     mode    => '0755',
     owner   => 'deployer',
@@ -76,94 +88,94 @@ class request {
     require => Package['apache2'],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/releases':
+  file { "${webroot}/releases":
     ensure  => directory,
     mode    => '0755',
     owner   => 'deployer',
     group   => 'deployer',
-    require => File['/var/www/request.tech.dreamhack.se'],
+    require => File[$webroot],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage':
+  file { "${webroot}/storage":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se'],
+    require => File[$webroot],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/app':
+  file { "${webroot}/storage/app":
     ensure  => directory,
     mode    => '0775',
     owner   => 'deployer',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage'],
+    require => File["${webroot}/storage"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/debugbar':
+  file { "${webroot}/storage/debugbar":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage'],
+    require => File["${webroot}/storage"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/framework':
+  file { "${webroot}/storage/framework":
     ensure  => directory,
     mode    => '0775',
     owner   => 'deployer',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage'],
+    require => File["${webroot}/storage"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/framework/cache':
+  file { "${webroot}/storage/framework/cache":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage/framework'],
+    require => File["${webroot}/storage/framework"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/framework/sessions':
+  file { "${webroot}/storage/framework/sessions":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage/framework'],
+    require => File["${webroot}/storage/framework"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/framework/views':
+  file { "${webroot}/storage/framework/views":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage/framework'],
+    require => File["${webroot}/storage/framework"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/storage/logs':
+  file { "${webroot}/storage/logs":
     ensure  => directory,
     mode    => '0775',
     owner   => 'deployer',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se/storage'],
+    require => File["${webroot}/storage"],
   }
 
-  file { '/var/www/request.tech.dreamhack.se/cache':
+  file { "${webroot}/cache":
     ensure  => directory,
     mode    => '0755',
     owner   => 'www-data',
     group   => 'www-data',
-    require => File['/var/www/request.tech.dreamhack.se'],
+    require => File[$webroot],
   }
   if ($dhid != {} and $db != {}) {
     file { 'env':
       ensure  => 'file',
-      path    => '/var/www/request.tech.dreamhack.se/.env',
+      path    => "${webroot}/.env",
       owner   => 'deployer',
       group   => 'www-data',
       mode    => '0740', # Use 0700 if it is sensitive
       content => template('request/env.erb'),
-      require => File['/var/www/request.tech.dreamhack.se'],
+      require => File[$webroot],
     }
   }
   exec { 'a2enmod_rewrite':
@@ -173,5 +185,27 @@ class request {
     notify  => Service['apache2'],
   }
 
+  # Needed for 'ssl-cert' group
+  ensure_packages(['ssl-cert'])
+
+  file { '/etc/ssl/certs/server-fullchain.crt':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0644',
+    source => 'puppet:///letsencrypt/fullchain.pem',
+    links  => 'follow',
+    notify => Service['apache2'],
+  }
+
+  file { '/etc/ssl/private/server.key':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0640',
+    source => 'puppet:///letsencrypt/privkey.pem',
+    links  => 'follow',
+    notify => Service['apache2'],
+  }
 
 }
