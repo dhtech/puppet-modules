378a30e53ba4df6ae590112e4106930da48ccce5
Alexander Malmström <jag@alexandermalmstrom.se>
2024-11-19 00:54:10 +0100

    Update init.pp

--- modules/wireguard/manifests/init.pp
+++ modules/wireguard/manifests/init.pp
@@ -2,6 +2,7 @@ class wireguard($current_event) {
   # Execute 'apt-get update'
   exec { 'apt-update':                    # exec resource named 'apt-update'
     command => '/usr/bin/apt-get update'  # command this resource will run
+    notify {"step 1":}
   }
 
   # Install wireguard package
