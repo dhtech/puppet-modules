21fb1294485622d70ae3101b5343f8bcb33041c2
Alexander Malmström <jag@alexandermalmstrom.se>
2024-11-19 00:15:43 +0100

    Wireguard (#408)
    * Update init.pp
    
    * puppet run order
    
    Fixed puppet run order
    Had missed to include current_event var
    
    * Update wireguard.py
    
    * lint fix
    
    * lint fix
    
    * lint

--- modules/wireguard.py
+++ modules/wireguard.py
@@ -2,9 +2,15 @@
 #
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file
-
+import lib
 
 def generate(host, *args):
-    return {'wireguard': None}
+    
+    # Get current event, used to get up-to-date switch conf
+    current_event = lib.get_current_event()
+
+    info = {}
+    info['current_event'] = current_event
+    return {'wireguard': info}
 
 # vim: ts=4: sts=4: sw=4: expandtab
