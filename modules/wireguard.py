344ea3d7bdd11766e5b3139111d907004385abff
Alexander Malmström <jag@alexandermalmstrom.se>
2024-11-19 13:28:47 +0100

    Update wireguard.py

--- modules/wireguard.py
+++ modules/wireguard.py
@@ -2,6 +2,8 @@
 #
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file
+
+#!/usr/bin/python3
 import lib
 import sqlite3
 import os
@@ -10,14 +12,41 @@ import os
 DB_FILE = '/etc/ipplan.db'
 
 def generate(host, *args):
-
+    # Get current event, used to get up-to-date switch conf
     current_event = lib.get_current_event()
 
-    tunnelip = '77.80.229.133/25'
+    if os.path.isfile(DB_FILE):
+        try:
+            conn = sqlite3.connect(DB_FILE)
+            db = conn.cursor()
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
+            sys.exit(2)
+    else:
+        print("No database file found: %s" % DB_FILE)
+        sys.exit(3)
+
+    db.execute('SELECT ipv4_netmask_dec FROM network WHERE short_name = "TECH-WIREGUARD-VPN";')
+    res = db.fetchone()
+    if not res:
+        raise NodeNotFoundError('Node %s not found' % host)
+
+    subnet = res[0]
+
+    db.execute('SELECT ipv4_gateway_txt FROM network WHERE short_name = "TECH-WIREGUARD-VPN";')
+    res = db.fetchone()
+    conn.close()
+    if not res:
+        raise NodeNotFoundError('Node %s not found' % host)
+    
+    gatewayIP = res[0]
+    tunnelIP = ipaddress.ip_address(gatewayIP) + 4
+    print(str(tunnelIP) + '/' + str(subnet))
+
+
     
     info = {}
     info['current_event'] = current_event
-    info['tunnelip'] = tunnelip
     return {'wireguard': info}
 
 # vim: ts=4: sts=4: sw=4: expandtab
