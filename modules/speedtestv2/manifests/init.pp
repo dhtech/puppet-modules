c93675271f3f34d9a48f96a99c574f862697110d
Alexander Malmström <jag@alexandermalmstrom.se>
2025-11-27 18:26:40 +0100

    Fixes the nginx config and adds SSL key

--- modules/speedtestv2/manifests/init.pp
+++ modules/speedtestv2/manifests/init.pp
@@ -57,4 +57,24 @@ class speedtestv2 {
     require => Package['nginx'],
   }
 
+  # Needed for 'ssl-cert' group
+  ensure_packages(['ssl-cert'])
+
+  file { '/etc/ssl/certs/speedtest2.event.dreamhack.se.crt':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0644',
+    source => 'puppet:///letsencrypt/fullchain.pem',
+    links  => 'follow',
+  }
+
+  file { '/etc/ssl/private/speedtest2.event.dreamhack.se.key':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0640',
+    source => 'puppet:///letsencrypt/privkey.pem',
+    links  => 'follow',
+  }
 }
