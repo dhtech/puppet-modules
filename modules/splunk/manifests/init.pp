493b505afc3678b83d7c5db3600540c158b93171
Alexander Malmström <jag@alexandermalmstrom.se>
2026-03-23 18:42:02 +0000

    Splunk test

---
+++ modules/splunk/manifests/init.pp
@@ -0,0 +1,19 @@
+class splunk{
+    file { '/etc/ssl/certs/splunk.event.dreamhack.se.crt':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0644',
+    source => 'puppet:///letsencrypt/fullchain.pem',
+    links  => 'follow',
+  }
+
+  file { '/etc/ssl/private/splunk.event.dreamhack.se.key':
+    ensure => file,
+    owner  => 'root',
+    group  => 'ssl-cert',
+    mode   => '0640',
+    source => 'puppet:///letsencrypt/privkey.pem',
+    links  => 'follow',
+  }
+}
